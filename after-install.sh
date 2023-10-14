37c1fd7bf71e457c8b6b1f55f79c176c407d65e8
차누 <chanoo@chanuui-MacBookAir.local>
2023-10-14 11:39:08 +0900

    code deploy 설정

--- after-install.sh
+++ after-install.sh
@@ -2,12 +2,12 @@ cd /home/ubuntu/blog
 docker stop blog
 docker rm blog
 docker image rm blog
-docker build -t blog .
+docker build -t blog -f ./blog.dockerfile .
 docker run -dit -p 30021:3000 --name blog blog
 
 cd /home/ubuntu/blog-admin
 docker stop blog-admin
 docker rm blog-admin
 docker image rm blog-admin
-docker build -t blog-admin .
-docker run -dit -p 30021:3000 --name blog-admin blog-admin
+docker build -t blog-admin -f ./blog-admin.dockerfile .
+docker run -dit -p 30022:3000 --name blog-admin blog-admin
